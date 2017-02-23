fe873ba9d90be6896a59ac56ec84fbd8afab4620
kbannach <krzysztof92b@gmail.com>
2017-02-23 19:51:50 +0100

    kod wywołujący OutOfMemoryException

---
+++ zad1/src/zad1/OOE.java
@@ -0,0 +1,14 @@
+package zad1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OOE {
+
+   public static void main(String[] args) {
+      List<Integer[]> tmp = new ArrayList<>();
+      while (true) {
+         tmp.add(new Integer[1024]);
+      }
+   }
+}
